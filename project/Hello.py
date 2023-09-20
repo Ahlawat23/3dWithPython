a17f15940422f068c68a402c13411d82c13240a8
PANKAJ AHLAWAT <pankajhlwt@gmail.com>
2023-09-20 22:58:23 +0530

    space bug solved

--- project/Hello.py
+++ project/Hello.py
@@ -3,3 +3,11 @@ pygame.init()
 screen_width = 1000
 screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
+
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+pygame.display.update()
+pygame.quit()
