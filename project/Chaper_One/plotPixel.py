4958cf079b2053452389d080e620ed9f100c4082
PANKAJ AHLAWAT <pankajhlwt@gmail.com>
2023-09-23 12:34:25 +0530

    made the leo constellation

--- project/Chaper_One/plotPixel.py
+++ project/Chaper_One/plotPixel.py
@@ -8,26 +8,20 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 done = False
 white = pygame.Color(255, 255, 255)
 
-
-
-
-
-
-def to_pygame_cordinates(display, x, y):
- return x, display.get_height() - y
-
-
 while not done:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    position = to_pygame_cordinates(screen, 100, 100)
-    pygame.draw.rect(screen, white, (position[0], position[1], 10, 10))
-
-    screen.set_at((100, 100), white)
-    screen.set_at((200, 200), white)
+    pygame.draw.rect(screen, white, (900, 250, 15, 15))
+    pygame.draw.rect(screen, white, (820, 200, 7, 7))
+    pygame.draw.rect(screen, white, (590, 270, 5, 5))
+    pygame.draw.rect(screen, white, (570, 600, 20, 20))
+    pygame.draw.rect(screen, white, (550, 330, 25, 25))
+    pygame.draw.rect(screen, white, (300, 580, 5, 5))
+    pygame.draw.rect(screen, white, (280, 350, 27, 27))
+    pygame.draw.rect(screen, white, (150, 560, 21, 21))
     pygame.display.update()
 
 pygame.quit()
