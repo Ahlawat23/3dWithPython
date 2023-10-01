ad15990113bd61c98fbc5bf82609e0178dc3c1f1
PANKAJ AHLAWAT <pankajhlwt@gmail.com>
2023-10-01 20:28:40 +0530

    added bresnham algo for drawing line

--- project/Chapter_3/LinePlot.py
+++ project/Chapter_3/LinePlot.py
@@ -1,4 +1,5 @@
 import pygame
+from  pygame.locals import *
 
 pygame.init()
 screen_width = 1000
@@ -11,10 +12,63 @@ green = pygame.Color(0, 255, 0)
 
 xoriginoffset = int(screen.get_width() / 2)
 yoriginoffset = int(screen.get_height() / 2)
+time_clicked = 0
+
+
+def plot_line(point1, point2):
+    x0, y0 = point1
+    x1, y1 = point2
+    dx = abs(x1 - x0)
+
+    if x0 < x1:
+        sx = 1
+    else:
+        sx = -1
+    dy = -abs(y1 - y0)
+    if y0 < y1:
+        sy = 1
+    else:
+        sy = -1
+    err = dx + dy
+
+    while True:
+        screen.set_at((x0, y0), white)
+        if x0 == x1 and y0 == y1:
+            break
+        e2 = 2 * err
+        if e2 >= dy:
+            err += dy
+            x0 += sx
+        if e2 <= dx:
+            err += dx
+            y0 += sy
+
+    while True:
+        screen.set_at((x0, y0), white)
+        if x0 == x1 and y0 == y1:
+            break
+        e2 = 2*err
+        if e2 >= dy:
+            err += dy
+            x0 += sx
+        if e2 >= dx:
+            err += dx
+            y0 = sy
+
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif event.type == MOUSEBUTTONDOWN:
+            if time_clicked == 0:
+                point1 = pygame.mouse.get_pos()
+            else:
+                point2 = pygame.mouse.get_pos()
+            time_clicked += 1
+            if time_clicked >1:
+                plot_line(point1, point2)
+                time_clicked = 0
+
     pygame.display.update()
 pygame.quit()
